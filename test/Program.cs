d3cad2ff1bc5f597602245f1bfde3b59efcb6523
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-09 22:56:17 +0200

    added Program.cs

---
+++ test/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+double cirkel1 = 3.14 * 3 * 3;
+double cirkel2 = 3.14 * 1 * 1;
+double cirkel3 = 3.14 * 5 * 5;
+
+Console.WriteLine($"cirkel1 = {cirkel1}");
+Console.WriteLine($"cirkel2 = {cirkel2}");
+Console.WriteLine($"cirkel3 = {cirkel3}");
