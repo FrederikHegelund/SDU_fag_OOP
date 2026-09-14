1295b27fd4a8d77bc744765e68e2a46f13f8765d
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-14 13:27:59 +0200

    updatering

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
-﻿Console.WriteLine("Celcius to Fharenhiet");
+﻿/*Console.WriteLine("Celcius to Fharenhiet");
 
 for (double c=0.5 ; c<=10.5 ; c++) {
     double fahrenheit = 32+(9.0/5.0)*c;
     Console.WriteLine("Celcius is " + c + "Fharenhiet" + fahrenheit);
 }
+*/
+// exercises 5.20 dice
+
+ int dice=2;
+
+ if (dice==2){
+     Console.WriteLine("winner");
+ }
+ else{
+     Console.WriteLine("loser");
+ }
