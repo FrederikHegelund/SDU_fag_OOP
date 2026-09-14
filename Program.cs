dd553adedce18dcf9c5ba6570b4e77dd54c7a679
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-14 13:57:13 +0200

    updatering

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ for (double c=0.5 ; c<=10.5 ; c++) {
     Console.WriteLine("Celcius is " + c + "Fharenhiet" + fahrenheit);
 }
 */
+
 // exercises 5.20 dice
 
+/*
  int dice=2;
 
  if (dice==2){
@@ -14,4 +16,17 @@ for (double c=0.5 ; c<=10.5 ; c++) {
  }
  else{
      Console.WriteLine("loser");
+ }
+ */
+
+ int jul = 21816000;
+ double pris = 599.95;
+ int dag =24 * 60 * 60;
+ int måned = 30 * dag;
+ int sekunder_jul = 12* måned;
+ if (jul >= sekunder_jul) {
+ Console.WriteLine(jul + "TV 50'' " + (599.95/30) + "pris" + pris);
+ }
+ else {
+ Console.WriteLine("gg_kidd");
  }
