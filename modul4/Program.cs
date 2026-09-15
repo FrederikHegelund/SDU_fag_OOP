673145d5997002ba71cec35ad3c9325633dfcd42
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-15 13:20:35 +0200

    nyt Program

---
+++ modul4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+int [] nummer = {1, 2, 3, 4, 5};
+int stor = nummer[0];
+int index = 0;
+
+for (int i = 1; i < nummer.Length; i++) {
+    if (nummer[i] > stor){
+        stor = nummer[i];
+        index = 1;
+    }
+}
+Console.WriteLine($"Stor: {stor}");
+Console.WriteLine($"index: {index}");
