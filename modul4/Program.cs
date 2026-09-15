42539512e4f3bcccc1535bfde10d2eeedf6e5867
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-15 13:52:00 +0200

    updatering

--- modul4/Program.cs
+++ modul4/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
-
+﻿
+/*
 int [] nummer = {1, 2, 3, 4, 5};
 int stor = nummer[0];
 int index = 0;
@@ -12,3 +12,21 @@ for (int i = 1; i < nummer.Length; i++) {
 }
 Console.WriteLine($"Stor: {stor}");
 Console.WriteLine($"index: {index}");
+*/
+
+class person {
+public int age;
+public int højde;
+public double vægt;
+}
+
+class program 
+{
+    static void Main()
+        {
+            person f = new person{ age = 21 , højde = 192 , vægt = 84.6};
+        Console.WriteLine("person f er " + f.age);
+        Console.WriteLine("person f er " + f.højde);
+        Console.WriteLine("person f er " + f.vægt);
+        }
+}
