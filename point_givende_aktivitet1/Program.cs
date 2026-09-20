65bbe7a1083ff0de83e81884820afb1c3197c3f4
Frederik Hegelund Christensen <fredc26@student.sdu.dk>
2026-09-20 18:49:21 +0200

    updatering

---
+++ point_givende_aktivitet1/Program.cs
@@ -0,0 +1,52 @@
+﻿// foreach loops if else 
+/*
+int [] tal = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+foreach(int nummer in tal)
+{
+    if (nummer == 10)
+    Console.WriteLine("BIG");
+    else 
+    Console.WriteLine(nummer);
+}
+*/
+
+//for statment, slektiv version
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+string[] biller = {"BMW", "FORD", "TESLA", "POLOSTAR"};
+
+for (int i=0; i < biller.Length; i++)
+        {
+            if ("BMW" == biller[0])
+            {
+            Console.WriteLine(biller[0]);
+            }
+            else{
+                Console.WriteLine("Kill yourself");
+            }
+            if ("FORD" == biller[3]){
+            Console.WriteLine("ja");
+            }
+            else {
+            Console.WriteLine("FORD er 2");
+            }
+        }
+    }
+}
+*/
+
+int[] year = {12, 23, 44, 43};
+
+int max = year[0];
+int min = year[1];
+
+for (int i=0; i< year.Length; i++)
+{
+    if (year[i] > max)
+    max = year[i];
+}
+Console.Write(max);
